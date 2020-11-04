a212ff896f505d7541cfc71759f803033c460172
y-du <42994541+y-du@users.noreply.github.com>
2020-11-04 13:04:39 +0100

    init

---
+++ manager/export.py
@@ -0,0 +1,110 @@
+"""
+   Copyright 2020 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
+__all__ = ("Handler",)
+
+
+from . import model
+from . import storage
+from .logger import getLogger
+from .util import getDelay
+import threading
+import datetime
+import requests
+import time
+import json
+import io
+
+
+logger = getLogger(__name__.split(".", 1)[-1])
+
+
+class ExportError(Exception):
+    pass
+
+
+class CreateExportError(ExportError):
+    pass
+
+
+class GetExportError(ExportError):
+    pass
+
+
+class ListExportsError(ExportError):
+    pass
+
+
+class RemoveExportError(ExportError):
+    pass
+
+
+class Handler(threading.Thread):
+    def __init__(self, endpoints: tuple, storage_handler: storage.Handler):
+        super().__init__(name="backup-worker", daemon=True)
+        self.__endpoints = endpoints
+        self.__st_handler = storage_handler
+
+    def __getData(self) -> dict:
+        data = dict()
+        for endpoint in self.__endpoints:
+            resp = requests.get(url=endpoint)
+            if not resp.ok:
+                raise RuntimeError("could not get data from '{}' - {}".format(endpoint, resp.status_code))
+            data[endpoint] = dict()
+            for key, value in resp.json():
+                data[endpoint][key] = json.loads(value)
+        return data
+
+    def createExport(self):
+        try:
+            stream = io.BytesIO()
+            json.dump(self.__getData(), stream)
+            self.__st_handler.write(stream, "{}Z".format(datetime.datetime.utcnow().isoformat()))
+        except Exception as ex:
+            raise CreateExportError("creating export failed - {}".format(ex))
+
+    def getExport(self, name: str):
+        try:
+            return self.__st_handler.read(name)
+        except FileNotFoundError:
+            raise
+        except Exception as ex:
+            raise GetExportError("retrieving export '{}' failed - {}".format(name, ex))
+
+    def listExports(self):
+        try:
+            return self.__st_handler.list()
+        except Exception as ex:
+            raise ListExportsError("listing exports failed - {}".format(ex))
+
+    def removeExport(self, name: str):
+        try:
+            return self.__st_handler.delete(name)
+        except FileNotFoundError:
+            raise
+        except Exception as ex:
+            raise RemoveExportError("removing export '{}' failed - {}".format(name, ex))
+
+    def run(self) -> None:
+        while True:
+            try:
+                time.sleep(getDelay())
+                logger.info("starting automatic export ...")
+                self.createExport()
+            except Exception as ex:
+                logger.error("automatic export failed - {}".format(ex))
